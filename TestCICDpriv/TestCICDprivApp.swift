dc45697cb0e71acc35d0d0c9b424765a659a6011
Tomasz Kubicki <tjpk@poczta.onet.pl>
2023-10-19 18:33:37 +0200

    Initial Commit

---
+++ TestCICDpriv/TestCICDprivApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCICDprivApp.swift
+//  TestCICDpriv
+//
+//  Created by Tomasz Kubicki on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestCICDprivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
